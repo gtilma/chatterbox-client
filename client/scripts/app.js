007390d48c3d7362a5aa32cb71e3814d10f33871
Student <student@MKS-SF-5.local>
2015-03-18 19:54:07 -0500

    Significant progress on chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,64 +1,98 @@
-// YOUR CODE HERE:
-var app = {};
+// $(document).ready(function() {
+  var app = {};
 
-app.init = function() {
-	this.url = "https://api.parse.com/1/classes/chatterbox",
-	this.contentType = "application/json"
-}
+  app.init = function() {
+  	this.server = "https://api.parse.com/1/classes/chatterbox",
+  	this.contentType = "application/json"
+  }
 
-app.init();
-//format messages as needed, also update styles.css
+  app.init();
+  //format messages as needed, also update styles.css
+  //display messages received from server
+  //use $.ajax to get everything from https://api.parse.com/1/classes/chatterbox
+  app.fetch = function() {
+  	$.ajax({
+  		url: app.server + '?order=-createdAt',
+  		type: "GET",
+  		contentType: app.contentType,
+  		success: function(data) {
+        //sanitize
+        for (var i = 0; i < data.results.length; i++) {
+          //console.log(data.results[i])
+          //function(message) {
+          if (data.results[i].hasOwnProperty("text")) {data.results[i].text = data.results[i].text.replace(/</g, "&lt;").replace(/>/g, "&gt;")}
+          if (data.results[i].hasOwnProperty("roomname")) {data.results[i].roomname = data.results[i].roomname.replace(/</g, "&lt;").replace(/>/g, "&gt;")}
+          if (data.results[i].hasOwnProperty("username")) {data.results[i].username = data.results[i].username.replace(/</g, "&lt;").replace(/>/g, "&gt;")}
+          addToRoom(data.results[i]);
+          // var room = data.results[i].roomname;
+          // if (!roomList.hasOwnProperty(room)) {
+          //   updateRoomList(room);
+          // }
+        }
+  		},
+  		error: function() {
+  			console.log("error")
+  		}
+  	});
+  };
 
-//display messages received from server
-//use $.ajax to get everything from https://api.parse.com/1/classes/chatterbox
-app.fetch = function(username) {
-	$.ajax({
-		url: app.url,
-		type: "GET",
-		//data: JSON.stringify(message),
-		contentType: app.contentType,
-		success: function(data) {
-			//$("#main").append(JSON.stringify(data));
-			console.log(data);
-		},
-		error: function() {
-			console.log("error")
-		}
-	});
-};
+  app.send = function(message) {
+  //send messages to parse server with post request
+  	$.ajax({
+  		url: app.server,
+  		type: "POST",
+  		data: JSON.stringify(message),
+  		contentType: app.contentType,
+  		success: function() {
+  			console.log("success")
+  		},
+  		error: function() {
+  			console.log("error")
+  		}
+  	});	
+  }
 
-app.send = function(message) {
-//send messages to parse server with post request
-	$.ajax({
-		// data: message,
-		url: app.url,
-		type: "POST",
-		data: JSON.stringify(message),
-		contentType: app.contentType,
-		success: function() {
+  	//probably have to parse the ajax somehow
+  	//sanitize?
 
-			console.log("success")
-		},
-		error: function() {
-			console.log("error")
-		}
-	});	
-}
+  //add messages to index.html, probably with append or prepend
+  var addToRoom = function (message) {
+    var box = $(document.createElement('div'));
+    box.addClass('message');
+    box.html(message.username + ': ' + message.text);
+    $('body').append(box);
+    // TODO : add timestamp
+    // TODO : add to pertinent room
+  };
 
-	//probably have to parse the ajax somehow
-	//sanitize?
-	//add messages to index.html, probably with append or prepend
+  $(document).on('click', '#submitMessage', function() {
+    var message = {};
+    message.roomname = $('#roomMenu option:selected').val();
+    message.text = $('#messageEntry').val();
+    message.username = $('#messageName').val();
+    app.send(message);
+    addToRoom(message);
+  });
 
-//refreshing messages - probably use a button
-	//either all messages from that time, or next 10
-
-//apply escaping to inputs
-	//something on keyup - use preventDefault to avoid <,/,], etc.?
-
-//allow users to select username
-	//apply username to all messages/inputs from that username
+  $(document).on('click', '#refresh', function() {
+    app.fetch();
+  });
+// });
 
 //button/input to create new room
-	//add room to dropdown list
-	//when list switches, show/hide rooms as appropriate
-	//apply room name to inputs, and place messages in appropriate room
+  //add room to dropdown list
+  //when list switches, show/hide rooms as appropriate
+  //apply room name to inputs, and place messages in appropriate room
+  // when adding a room, update room list & add new message container
+  // loop through data & check for message container div (update list if !exists container)
+
+//prettification (CSS)
+
+//refreshing messages - probably use a button
+  //either all messages from that time, or next 10
+
+//apply escaping to inputs
+  // sanitize on send as well as fetch
+
+//allow users to select username
+  //apply username to all messages/inputs from that username
